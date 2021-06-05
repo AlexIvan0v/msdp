0a037386c96f97ef902d21a937dcf305be912348
Oleksii Ivanov <Oleksii.Ivanov@cantire.com>
2021-06-05 17:57:13 +0300

    update for linux instalation

--- Requirements_instalation_Linux.sh
+++ Requirements_instalation_Linux.sh
@@ -1,32 +1,20 @@
+sudo apt update
+sudo apt install snapd
 sudo apt-get install python3 -y
-
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm -y
-
-
-
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+sudo snap install helm --classic
+sudo snap install kubectl --classic
 
 
 #Azure cli instalation
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az --version
 
-
 #Install AWS cli and eksctl
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
+sudo apt  install awscli 
 
 #installing gcloud 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get install apt-transport-https ca-certificates gnupg
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk -y
+sudo snap install google-cloud-sdk --classic
