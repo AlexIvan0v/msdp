ad57eee5812242eb1db777522a8882367e263fc4
Oleksii Ivanov <Oleksii.Ivanov@cantire.com>
2021-06-05 17:58:28 +0300

    fix up

--- Requirements_instalation_Linux.sh
+++ Requirements_instalation_Linux.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install snapd
+sudo apt install snapd -y
 sudo apt-get install python3 -y
 sudo snap install helm --classic
 sudo snap install kubectl --classic
@@ -14,7 +14,7 @@ az --version
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-sudo apt  install awscli 
+sudo apt  install awscli -y
 
 #installing gcloud 
 sudo snap install google-cloud-sdk --classic
